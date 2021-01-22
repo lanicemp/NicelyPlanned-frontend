badeab5806fca9ca58f38ce0e6c82192d4d61650
LaNice Powell <lanicemp@aol.com>
2021-01-21 19:06:47 -0500

    created ers for reducers

---
+++ src/reducers/users.js
@@ -0,0 +1,9 @@
+export default (state = [], action) => {
+  switch ((action.type)) {
+    default:
+      return state;
+  }
+};
+// the action is an object
+// the state must have a default value of whatever it returns
+// when the app launches it return the default value of all reducers.
