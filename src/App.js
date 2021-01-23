6047299d1131da36b1019a97386804a9092bc195
LaNice Powell <lanicemp@aol.com>
2021-01-22 23:42:18 -0500

    added login import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import Login from './components/Login'
 
 class App extends React.Component {
   render(){
     return (
-     "Hello I'm React"
+     <Login/>
     );
   }
   
