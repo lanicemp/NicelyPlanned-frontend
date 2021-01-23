2d110ff0b84e6a96e45cf348514fc3ea66bfe868
LaNice Powell <lanicemp@aol.com>
2021-01-22 23:53:22 -0500

    created login form reducer

---
+++ src/reducers/loginForm.js
@@ -0,0 +1,14 @@
+const initialState = {
+    email: "",
+    password: ""
+}
+
+
+export default (state = initialState, action) => {
+    switch ((action.type)) {
+        case "UPDATE_LOGIN_FORM":
+            return action.user
+      default:
+        return state;
+    }
+  };
