a369408bf6123e0e54e64f36963ecf409f65b654
LaNice Powell <lanicemp@aol.com>
2021-01-24 20:14:16 -0500

    spelling error

--- src/actions/loginForm.js
+++ src/actions/loginForm.js
@@ -1,5 +1,5 @@
 //action creators points to a function that returns an action. 
-export const updateLoginForm = FormData => {
+export const updateLoginForm = (formData) => {
     return{
         type: "UPDATE_LOGIN_FORM",
         formData
