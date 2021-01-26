31afc2a6cc46fd06e467e9b91f6828791b1b9163
LaNice Powell <lanicemp@aol.com>
2021-01-25 22:44:58 -0500

    updated

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -7,7 +7,7 @@ const initialState = {
 export default (state = initialState, action) => {
     switch ((action.type)) {
         case "UPDATE_LOGIN_FORM":
-            return action.user
+            return action.formData
       default:
         return state;
     }
