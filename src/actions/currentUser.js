872205e540ac9259cbc5e3d70c3fa56d7b114bff
LaNice Powell <lanicemp@aol.com>
2021-01-26 14:23:10 -0500

    added console.log

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -13,6 +13,7 @@ export const login = (credentials) => {
   return (dispatch) => {
     return fetch("http://localhost:3000/api/v1/login", {
       credentials:"include",
+      
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -31,6 +32,7 @@ export const login = (credentials) => {
 // This returns a function that retuns a fecth that I can dispatch as I see fit. 
 
 export const getCurrentUser = () => {
+  console.log("in action creator getCurrentUser")
   
   return (dispatch) => {
     return fetch("http://localhost:3000/api/v1/get_current_user", {
