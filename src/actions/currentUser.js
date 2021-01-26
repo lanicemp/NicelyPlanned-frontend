0772f539e6b0b4b7eab26ae66a76b21d090cfcdb
LaNice Powell <lanicemp@aol.com>
2021-01-26 00:55:33 -0500

    updated login and created getCurrentUser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -12,12 +12,40 @@ export const login = (credentials) => {
   console.log("credentials are", credentials)
   return (dispatch) => {
     return fetch("http://localhost:3000/api/v1/login", {
+      credentials:"include",
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify()
-    });
+      body: JSON.stringify(credentials)
+    })
+    .then(r => r.json())
+    .then( user => {
+      if (user.error){
+        alert(user.error)
+      } else {dispatch(setCurrentUser(user))}
+    })
+    .catch(console.log)
   };
 };
 // This returns a function that retuns a fecth that I can dispatch as I see fit. 
+
+export const getCurrentUser = () => {
+  
+  return (dispatch) => {
+    return fetch("http://localhost:3000/api/v1/get_current_user", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then(r => r.json())
+    .then( user => {
+      if (user.error){
+        alert(user.error)
+      } else {dispatch(setCurrentUser(user))}
+    })
+    .catch(console.log)
+  };
+};
