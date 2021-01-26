1bb386db290ca3a73bf6142fd9897e022c98d085
LaNice Powell <lanicemp@aol.com>
2021-01-26 14:24:28 -0500

    added imports and connect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css';
 import Login from './components/Login'
+import {connect} from 'react-redux'
+import {getCurrentUser} from "./actions/currentUser"
 
 class App extends React.Component {
 
 componentDidMount(){
-  
+  console.log("in component did Mount")
+  this.props.getCurrentUser()
+
 }
 
   render(){
@@ -16,4 +20,4 @@ componentDidMount(){
   
 }
 
-export default App;
+export default connect(null, {getCurrentUser})(App);
