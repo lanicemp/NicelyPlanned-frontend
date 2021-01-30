8da3bad24ed4ebf3a76e9c3d581a83b5144027b7
LaNice Powell <lanicemp@aol.com>
2021-01-29 23:35:39 -0500

    updat mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import './App.css';
 import Login from './components/Login'
+import Logout from './components/Logout'
 import {connect} from 'react-redux'
 import {getCurrentUser} from "./actions/currentUser"
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 class App extends React.Component {
 
 componentDidMount(){
   console.log("in component did Mount")
   this.props.getCurrentUser()
+  console.log(this.props)
 
 }
 
   render(){
     return (
-     <Login/>
+    this.props.currentUser ? <Logout/> : <Login/>
     );
   }
   
 }
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  
+  })
+}
 
-export default connect(null, {getCurrentUser})(App);
+export default connect(mapStateToProps, { getCurrentUser })(App);
