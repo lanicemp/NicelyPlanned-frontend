5177c7a05bdd9f6741f37c618dc67ab6644d6916
LaNice Powell <lanicemp@aol.com>
2021-01-30 12:03:53 -0500

    added Browser Router

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={store}><BrowserRouter>
+    <App /></BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
