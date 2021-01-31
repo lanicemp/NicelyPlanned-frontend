754323e765969b0a41ed4659aed68c3db2265896
LaNice Powell <lanicemp@aol.com>
2021-01-31 15:15:04 -0500

    created main container

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const MainContainer = () => {
+  return (
+    <div className="MainContainer">
+     <p> Im in Main Container</p>
+    </div>
+  )
+}
+
+export default MainContainer
