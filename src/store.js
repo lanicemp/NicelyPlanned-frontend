2a61e26f59a906ba0b19dfce26e059d644908bd3
LaNice Powell <lanicemp@aol.com>
2021-01-31 15:16:13 -0500

    add meetings to the reducer

--- src/store.js
+++ src/store.js
@@ -1,14 +1,18 @@
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
-import thunk  from 'redux-thunk'
-import usersReducer from './reducers/users'
-import currentUser from './reducers/currentUser'
-import loginForm from './reducers/loginForm'
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import usersReducer from "./reducers/users";
+import currentUser from "./reducers/currentUser";
+import loginForm from "./reducers/loginForm";
+import myMeetings from "./reducers/myMeetings";
 
 const reducer = combineReducers({
-    users: usersReducer,currentUser, loginForm
-  })
-  
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk))) 
+  users: usersReducer,
+  currentUser,
+  loginForm,
+  myMeetings,
+});
 
-   export default store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
