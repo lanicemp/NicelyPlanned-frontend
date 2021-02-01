fb0137361b1619e1e8d24b95d20eb6c817a4f693
LaNice Powell <lanicemp@aol.com>
2021-02-01 13:02:33 -0500

    inital commit

---
+++ src/components/Signup.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { signup } from "../actions/currentUser.js"
+import { updateSignupForm } from "../actions/signupForm.js"
+
+const Signup = ({ signupFormData, updateSignupForm, signup, history }) => {
+
+    const handleUserInfoInputChange = event => {
+      const { name, value } = event.target
+      const updatedFormInfo = {
+        ...signupFormData,
+        [name]: value
+      }
+      updateSignupForm(updatedFormInfo)
+    }
+    const handleSubmit = event => {
+        event.preventDefault()
+        signup(signupFormData, history)
+      }
+      return (
+        <div className="Signup">
+          <h2> Sign Up Below </h2>
+        <form onSubmit={handleSubmit}>
+          <input placeholder="name" value={signupFormData.name} name="name" type="text" onChange={handleUserInfoInputChange} />
+          <input placeholder="email" value={signupFormData.email} name="email" type="text" onChange={handleUserInfoInputChange} />
+          <input placeholder="password" value={signupFormData.password_digest} name="password_digest" type="text" onChange={handleUserInfoInputChange} />
+         <input type="submit" value="Sign Up"/>
+        </form>
+        </div>
+      )
+    }
+    const mapStateToProps = state => {
+        return {
+          signupFormData: state.signupForm
+        }
+      }
+      
+      export default connect(mapStateToProps, { updateSignupForm, signup } )(Signup)
