6d2a88ac9476448bb9211a94fc6d700b8c900111
LaNice Powell <lanicemp@aol.com>
2021-01-31 21:21:03 -0500

    created getMyMeetings function

--- src/actions/myMeetings.js
+++ src/actions/myMeetings.js
@@ -4,3 +4,30 @@ export const setMyMeetings = (meetings) => {
       meetings,
     };
   };
+
+
+
+
+
+
+  //async actions 
+  export const getMyMeetings = () => {
+    return dispatch => {
+      return fetch("http://localhost:3000/api/v1/meetings", {
+        credentials: "include",
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+        .then(r => r.json())
+        .then(response => {
+          if (response.error) {
+            alert(response.error)
+          } else {
+            dispatch(setMyMeetings(response.data))
+          }
+        })
+        .catch(console.log)
+    }
+  }
