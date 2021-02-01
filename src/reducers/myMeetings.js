2929e813ec60ce01291524281f2e2151a0e5ea27
LaNice Powell <lanicemp@aol.com>
2021-01-31 20:53:52 -0500

    added intitial state and case

--- src/reducers/myMeetings.js
+++ src/reducers/myMeetings.js
@@ -1,5 +1,9 @@
-export default (state = [], action) => {
+const initialState = []
+
+export default (state = initialState, action) => {
     switch (action.type) {
+      case "SET_MY_MEETINGS":
+        return action.meetings
      
       default:
         return state;
