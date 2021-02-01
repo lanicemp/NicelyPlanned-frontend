c1d3285291db1249c43b51662a873f65ab74a78e
LaNice Powell <lanicemp@aol.com>
2021-01-31 21:18:55 -0500

    imported myMeeting component and reducer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import myMeetings from '../reducers/myMeetings'
+import MyMeetings from './MyMeetings'
 
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
      <p> Im in Main Container</p>
+     <MyMeetings/>
     </div>
   )
 }
