6a4b6d6a282f52fe87fbf1a8fb01b7acabe9f1e1
LaNice Powell <lanicemp@aol.com>
2021-02-01 21:10:30 -0500

    added update, reset and edit actions

---
+++ src/actions/meetingForm.js
@@ -0,0 +1,25 @@
+export const updatemeetingForm = (name, value) => {
+    const formData = { name, value }
+    return {
+      type: "UPDATE_NEW_MEETING_FORM",
+      formData
+    }
+  }
+  
+  export const resetmeetingForm = () => {
+    return {
+      type: "RESET_NEW_MEETING_FORM",
+    }
+  }
+  
+  export const setFormDataForEdit = meeting => {
+    const meetingFormData = {
+      name: meeting.attributes.name,
+      startDate: meeting.attributes.start_date,
+      endDate: meeting.attributes.end_date
+    }
+    return {
+      type: "SET_FORM_DATA_FOR_EDIT",
+      meetingFormData
+    }
+  }
