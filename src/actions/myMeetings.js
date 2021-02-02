5c5ec346490be1ad911e311abf223555ee06bc5a
LaNice Powell <lanicemp@aol.com>
2021-02-02 00:00:45 -0500

    addded createMeeting

--- src/actions/myMeetings.js
+++ src/actions/myMeetings.js
@@ -31,3 +31,39 @@ export const setMyMeetings = (meetings) => {
         .catch(console.log)
     }
   }
+
+  export const createMeeting = (meetingData, history) => {
+    return dispatch => {
+      const sendableMeetingData = {
+        start_time: meetingData.startDate,
+        end_time: meetingData.endDate,
+        title: meetingData.title,
+        detail: meeftingData.detail,
+        comment: meetingData.comment, 
+        date: meetingData.date,
+        user_id: meetingData.userId
+      }
+      return fetch("http://localhost:3000/api/v1/meetings", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(sendableMeetingData)
+      })
+        .then(r => r.json())
+        .then(resp => {
+          if (resp.error) {
+            alert(resp.error)
+          } else {
+            dispatch(addMeeting(resp.data))
+            dispatch(resetMeetingForm())
+            history.push(`/meetings/${resp.data.id}`)
+            // go somewhere else --> meeting show?
+            // add the new meeting to the store
+          }
+        })
+        .catch(console.log)
+  
+    }
+  }
