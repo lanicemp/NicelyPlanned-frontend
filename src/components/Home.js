a87c254bf64019d9a14da59a7278f63c137e03d3
LaNice Powell <lanicemp@aol.com>
2021-02-01 21:09:54 -0500

    updated header

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 
 const Home = () => (
   <div>
+      <h1>Welcome to Nicely Planned</h1>
+      <h3>Your personal daily planner</h3>
     <span>
       <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link>
     </span>
