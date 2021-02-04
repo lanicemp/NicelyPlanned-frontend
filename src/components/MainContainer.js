2eb1470b845b16113782a3e1e4ae5ee28172e0a7
LaNice Powell <lanicemp@aol.com>
2021-02-03 20:29:48 -0500

    ...

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
 import MyMeetings from './MyMeetings'
 
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
+
      <p> Im in Main Container</p>
      
      <MyMeetings/>
