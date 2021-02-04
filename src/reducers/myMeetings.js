87bdeb90f867a8882248a6f378c8b8961ee7e021
LaNice Powell <lanicemp@aol.com>
2021-02-04 01:02:43 -0500

    added Add Meeting

--- src/reducers/myMeetings.js
+++ src/reducers/myMeetings.js
@@ -4,7 +4,8 @@ export default (state = initialState, action) => {
     switch (action.type) {
       case "SET_MY_MEETINGS":
         return action.meetings
-     
+        case "ADD_MEETING":
+          return state.concat(action.trip)
       default:
         return state;
     }
