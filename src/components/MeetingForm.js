fbe7438436c59b935713d8b4c20138b8a8366c3a
LaNice Powell <lanicemp@aol.com>
2021-02-04 01:04:31 -0500

    added labels to form and console.log to track data

--- src/components/MeetingForm.js
+++ src/components/MeetingForm.js
@@ -5,10 +5,11 @@ import { connect } from 'react-redux'
 
 
 const MeetingForm = ({ formData, updateMeetingForm, userId, meeting, handleSubmit, editMode }) => {
+  console.log("userId in meeting Form ", userId)
   //
 
 
-    const  { title, start_time, end_time, date, detail, comment } = formData
+    const  { title, start_time, end_time, date, detail, comment} = formData
   
     const handleChange = event => {
       console.log("trigger Handle change")
@@ -24,42 +25,53 @@ const MeetingForm = ({ formData, updateMeetingForm, userId, meeting, handleSubmi
           event.preventDefault()
           handleSubmit(formData)
         }}>
+          <label>
+            Title:
           <input
             placeholder="title"
             name="title"
             onChange={handleChange}
             value={title}
-          /><br/> 
-          <input
+          /></label><br/> 
+         <label> Detail:<input
           placeholder="detail"
           name="detail"
           onChange={handleChange}
           value={detail}
-        /><br/>
+        /></label><br/>
+         <label> Date:
          <input
             placeholder="date"
             name="date"
+            type="date"
             onChange={handleChange}
             value={date}
-          /><br/>
-           <input
+          /></label><br/>
+            <label> Comment:<input
             placeholder="comment"
             name="comment"
             onChange={handleChange}
             value={comment}
-          /><br/>
-          <input
+          /></label><br/>
+          <label> Start Time: <input
             placeholder="start time"
             name="start_time"
+            type="time"
             onChange={handleChange}
             value={start_time}
-          /><br/>
-          <input
-            placeholder="End Time "
-            name="endTime "
+          /></label><br/>
+          <label> End Time:<input
+            placeholder="end time "
+            name="end_time"
+            type="time"
             onChange={handleChange}
             value={end_time}
-          /><br/>
+          /></label><br/>
+          <input 
+          name="user_id"
+          type="hidden"
+          value={userId }
+          />
           <input
             type="submit"
             value={editMode ? "Update Meeting" : "Create Meeting" }
@@ -69,6 +81,7 @@ const MeetingForm = ({ formData, updateMeetingForm, userId, meeting, handleSubmi
     
     const mapStateToProps = state => {
       const userId = state.currentUser ? state.currentUser.id : ""
+      console.log("set state userId", userId)
       return {
         formData: state.meetingForm,
         userId
