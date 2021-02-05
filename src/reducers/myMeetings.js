2ac7e97543b38f0c17406e95549ca1e0f7b3c9cc
LaNice Powell <lanicemp@aol.com>
2021-02-05 11:35:44 -0500

    added update delete and cleat reducers

--- src/reducers/myMeetings.js
+++ src/reducers/myMeetings.js
@@ -5,7 +5,14 @@ export default (state = initialState, action) => {
       case "SET_MY_MEETINGS":
         return action.meetings
         case "ADD_MEETING":
-          return state.concat(action.trip)
+          return state.concat(action.meeting)
+          case "UPDATE_MEETING":
+            return state.map(meeting => meeting.id === action.meeting.id ? action.meeting : meeting)
+          case "DELETE_MEETING":
+            //console.log( "action.meetingId is ", action.meetingId)
+            return state.filter(meeting => meeting.id === action.meetingId ? false : true)
+          case "CLEAR_MEETINGS":
+            return initialState
       default:
         return state;
     }
