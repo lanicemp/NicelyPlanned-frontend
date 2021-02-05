6d2ce46f5cf2ab95f54ea99d6fd7e040e3a3b5eb
LaNice Powell <lanicemp@aol.com>
2021-02-05 11:37:05 -0500

    updated meeting card to reflect attributes

--- src/components/MeetingCard.js
+++ src/components/MeetingCard.js
@@ -5,9 +5,9 @@ const MeetingCard = ({ meeting }) => {
   return (
     meeting ?
       <div>
-        <h3>{meeting.attributes.name}</h3>
-        <p>{meeting.attributes.start_date}</p>
-        <p>{meeting.attributes.end_date}</p>
+        <h3>{meeting.attributes.title}</h3>
+        <p>{meeting.attributes.start_time}</p>
+        <p>{meeting.attributes.end_time}</p>
         <Link to={`/meetings/${meeting.id}/edit`}>Edit this meeting</Link>
       </div> :
       <p>This the the Meeting card with no meeting!</p>
