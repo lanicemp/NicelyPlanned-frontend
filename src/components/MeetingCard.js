0fdd9ee8af576973bf0586fd8ff1d64f9eb98534
LaNice Powell <lanicemp@aol.com>
2021-02-07 23:52:24 -0500

    added console.log for tracking

--- src/components/MeetingCard.js
+++ src/components/MeetingCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const MeetingCard = ({ meeting }) => {
+  console.log("meeting in Card", meeting)
   return (
     meeting ?
       <div>
