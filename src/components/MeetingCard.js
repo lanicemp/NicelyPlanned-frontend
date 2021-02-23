53d4aa693270f6c91bf946cd834b120566132913
LaNice Powell <lanicemp@aol.com>
2021-02-23 13:58:56 -0500

    added moment to fix time

--- src/components/MeetingCard.js
+++ src/components/MeetingCard.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, StaticRouter} from 'react-router-dom'
+import moment from 'moment';
 
 const MeetingCard = ({ meeting }) => {
 
-  const date= new Date(meeting.attributes.start_time) 
+  let  time= new Date(meeting.attributes.start_time) ;
+  let startTime = moment(time).add(5,"hours").format("LT")
+  let etime = new Date(meeting.attributes.end_time)
+  let endTime = moment(etime).add(5,"hours").format("LT")
+  let date = moment(meeting.attributes.date).format("MM/DD/YYYY")
+  console.log("moment date", moment(meeting.attributes.date).format("MM/DD/YYYY"))
+  const now = new Date()
   console.log("meeting in Card", meeting)
-  console.log("date", date)
+
   return (
     meeting ?
       <div>
         <h3>{meeting.attributes.title}</h3>
-        <p>{meeting.attributes.start_time}</p>
-        <p>{meeting.attributes.end_time}</p>
+        <h4>Date of Event: {date}</h4>
+        <p>Start time: {startTime}</p>
+        <p>End time: {endTime}</p>
+        <p>Details: {meeting.attributes.detail}</p>
+        <p>Comment: {meeting.attributes.comment}</p>
         <Link to={`/meetings/${meeting.id}/edit`}>Edit this meeting</Link>
       </div> :
       <p>This the the Meeting card with no meeting!</p>
