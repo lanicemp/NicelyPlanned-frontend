3d142f5111e47365ab9381b357f2b3b1816d92aa
LaNice Powell <lanicemp@aol.com>
2021-03-04 10:36:59 -0500

    added props, update the state

--- src/components/MyMeetings.js
+++ src/components/MyMeetings.js
@@ -1,36 +1,56 @@
-import React, {Componet} from "react";
+import React from 'react';
+// import React, {Componet} from "react";
 import {getMyMeetings} from "../actions/myMeetings"
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-const MyMeetings = props => {
-  console.log("MyMeeting component props", props)
-    // const meetingCards = props.meetings.length > 0 ?
-    // props.meetings.map(t => (<p key={t.id}><Link to={`/meetings/${t.id}`}>{t.attributes.name}</Link></p>)) :
-    // null
+class MyMeetings extends React.Component {
+  
+  componentDidMount(){
+    // this.props.getMyMeetingsWithDispatch()
+    this.props.getMyMeetings()
+  
+  }
     
-    let today = new Date()
+  
+   
+  
+
+ render(){
+ 
+    console.log("this.props", this.props)
+
+     let today = new Date()
      let dd = String(today.getDate()).padStart(2, '0');
      let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
      let  yyyy = today.getFullYear();
      today = mm + '/' + dd + '/' + yyyy;
 
-  
 
- 
+    const meetingCards = this.props.meetings.length > 0 ?
+    this.props.meetings.map(t => (<p key={t.id}><Link to={`/meetings/${t.id}`}>{t.title}</Link></p>)) :
+    null
+
+
   return(
       <p> in My Meetings</p>,
-      <h3>{today}</h3>
-      //  meetingCards
+      <h3>{today}</h3>,
+       meetingCards
     
 
   )
-
+ }
   
 }
 
+// const mapDispatchToProps = (dispatch) => { 
+//   return {
+//     getMyMeetingsWithDispatch: () => dispatch(getMyMeetings())
+//   }
+// }
 
-
+//connect is the bridge to the react componet and redux
+//dispatch makes a copy of my action creator with dispatch functionality. 
 
 
 // we provide mapStateToProps to Redux in order to tell Redux:
@@ -41,8 +61,8 @@ const MyMeetings = props => {
 const mapStateToProps = state => {
   console.log("state in MyMeetings", state)
   return {
-    meetings: state.meetings
+    meetings: state.myMeetings
   }
 }
 
-export default connect(mapStateToProps)(MyMeetings)
+export default connect(mapStateToProps, {getMyMeetings})(MyMeetings)
